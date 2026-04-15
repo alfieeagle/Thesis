ad0d3430ed717d6762d92fa5eea91fc07788c028
alfieeagle <alfieeagle@gmail.com>
2026-04-15 17:07:41 +1000

    Added global pin definition config. Added the depth sensor library. Setup the USB, UART, depth sensor and all digital pins in main.cpp

---
+++ firmware/include/pin_definitions.h
@@ -0,0 +1,15 @@
+#ifndef PIN_DEFINITIONS_H
+
+#define PIN_DEFINITIONS_H
+
+#define INDEX            2  // Index each full step of the motor
+#define LIM_RET          3  // Retracted limit switch
+#define LIM_EXT          4  // Extended limit switch
+#define EN_PIN           5  // Enable driver
+#define DIR_PIN          18 // Direction
+#define STEP_PIN         19 // Step
+#define SERIAL_PORT Serial1 // HardwareSerial port
+#define DRIVER_ADDRESS 0b00 // TMC2209 Driver address according to MS1 and MS2
+#define R_SENSE 0.11f // Internal resistance
+
+#endif
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -9,40 +9,67 @@ This file contains the main entry point of the VBS program which
 communicates with sensors, controls the VBS hardware and reports
 back via USB to an external computer.
 
-Dependencies:	TMCStepper.h, Arduino.h, VBS.hpp
+Dependencies:	TMCStepper.h, Arduino.h, vbs.hpp, pin_definitions.h
+              MS5837.h
 
 **/
-
 
 #include <TMCStepper.h>
 #include <Arduino.h>
 
+#include "vbs.hpp"
+#include "pin_definitions.h"
+#include "MS5837.h"
 
-#define DIR_PIN          18 // Direction
-#define STEP_PIN         19 // Step
-#define SERIAL_PORT Serial1 // HardwareSerial port
-#define DRIVER_ADDRESS 0b00 // TMC2209 Driver address according to MS1 and MS2
-
-#define R_SENSE 0.11f 
-
-TMC2209Stepper driver(&SERIAL_PORT, R_SENSE, DRIVER_ADDRESS);                    // Hardware Serial
+TMC2209Stepper driver(&SERIAL_PORT, R_SENSE, DRIVER_ADDRESS);   // Hardware Serial
+MS5837 sensor;
 
 void setup() {
-  // pinMode(EN_PIN, OUTPUT);
+  // Start USB coms
+  Serial.begin(9600);
+  Serial.println("Starting");
+
+  Wire.begin();
+
+  // Initialize pressure sensor
+  // Returns true if initialization was successful
+  // Wire1 corresponds with PINS 16 (SCL) and 17 (SDA) on Teensy 4.1
+  while (!sensor.init(Wire1)) {
+    Serial.println("Init failed!");
+    Serial.println("Are SDA/SCL connected correctly?");
+    Serial.println("Blue Robotics Bar30: White=SDA, Green=SCL");
+    Serial.println("\n\n\n");
+    delay(5000);
+  }
+
+  // Select 30 bar model of depth sensor
+  sensor.setModel(MS5837::MS5837_30BA);
+
+  // 997 for freshwater
+  sensor.setFluidDensity(1025);
+
+  // Setup output pins
+  pinMode(EN_PIN, OUTPUT);
   pinMode(STEP_PIN, OUTPUT);
   pinMode(DIR_PIN, OUTPUT);
-  // digitalWrite(EN_PIN, LOW);      // Enable driver in hardware
+
+  // Setup input pins
+  pinMode(INDEX, INPUT);
+  pinMode(LIM_EXT, INPUT);
+  pinMode(LIM_RET, INPUT);
+
+  // Disable driver initially 
+  digitalWrite(EN_PIN, HIGH);
 
   SERIAL_PORT.begin(115200);      // HW UART drivers
 
-  driver.begin();                 //  SPI: Init CS pins and possible SW SPI pins
-                                  // UART: Init SW UART (if selected) with default 115200 baudrate
+  driver.begin();                 // UART: Init SW UART with default 115200 baudrate
   driver.toff(5);                 // Enables driver in software
-  driver.rms_current(1000);        // Set motor RMS current
-  driver.microsteps(0);          // Set microsteps to full steps 
+  driver.rms_current(1000);       // Set motor RMS current
+  driver.microsteps(0);           // Set microsteps to full steps 
 
-driver.en_spreadCycle(true);   // Toggle spreadCycle on TMC2208/2209/2224
-driver.pwm_autoscale(true);     // Needed for stealthChop
+driver.en_spreadCycle(true);      // Toggle spreadCycle
+driver.pwm_autoscale(true);       // Needed for stealthChop
 }
 
 void loop() {
